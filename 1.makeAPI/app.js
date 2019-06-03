aa4f71612bec22ce47638ffb47dbc6539f49def0
lzy565418918 <565418918@qq.com>
2019-06-03 13:33:36 +0800

    基本功能实现

--- 1.makeAPI/app.js
+++ 1.makeAPI/app.js
@@ -6,15 +6,27 @@ var bodyParser = require('body-parser')
 //导路径包
 const path = require('path')
 //导数据
-const db = require(path.join(__dirname,'utils','db.js'))
+const db = require(path.join(__dirname, 'utils', 'db.js'))
+//导可以传文件的包
+const multer = require('multer')
+//用户上传的文件保存在这里
+const upload = multer({
+    dest: 'uploads/'
+})
+
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 //注册路由  用户登录  有参数  post
 app.post('/login', (req, res) => {
     //获取请求参数
-    const { username, password } = req.body
+    const {
+        username,
+        password
+    } = req.body
     //判断请求的用户名和密码，正确就返回成功，错误就返回失败
     if (username == 'admin' && password == '123456') {
         res.send({
@@ -25,13 +37,13 @@ app.post('/login', (req, res) => {
         res.send({
             msg: '用户名或密码错误',
             code: 400
-            
+
         })
     }
 })
 
 
-//获取英雄列表，无参数  get  
+//注册路由  获取英雄列表，无参数  get  
 app.get('/list', (req, res) => {
     //导入已经写好的数据，调用db的getheros方法
     const data = db.getHeros()
@@ -42,6 +54,92 @@ app.get('/list', (req, res) => {
     })
 })
 
+//注册路由  新增英雄信息，有参数（icon,name,skill） post  formData
+app.post('/add', upload.single('icon'), (req, res) => {
+    const icon = req.file.path
+    const {
+        name,
+        skill
+    } = req.body
+    if (db.addHero({
+            name,
+            skill,
+            icon
+        })) {
+        res.send({
+            msg: "新增成功",
+            code: 200
+        })
+    } else {
+        res.send({
+            msg: "参数错误",
+            code: 400
+        })
+    }
+
+})
+
+
+//注册路由  删除英雄信息，有参数 id get
+app.get('/delete', (req, res) => {
+    const id = req.query.id
+    if (db.deleteHerobyId(id)) {
+        res.send({
+            msg: '删除成功',
+            code: 200
+        })
+    } else {
+        res.send({
+            msg: '删除失败',
+            code: 200
+        })
+    }
+
+})
+
+//注册路由  根据id查找英雄，有参数id get
+app.get('/search', (req, res) => {
+    const id = req.query.id
+    if (db.getHeroById(id)) {
+        res.send({
+            msg: '查找成功',
+            code: 200
+        })
+    } else {
+        res.send({
+            msg: '查找失败',
+            code: 400
+        })
+    }
+})
+
+
+//注册路由 编辑英雄信息 有参数 id post
+app.post('/edit', upload.single('icon'), (req, res) => {
+    // req.file的path属性里放着icon图片的地址
+    const icon = req.file.path
+    const {
+        name,
+        skill,
+        id
+    } = req.body
+    if (db.editHero({
+            name,
+            skill,
+            icon
+        })) {
+        res.send({
+            msg: "修改成功",
+            code: 200
+        })
+    } else {
+        res.send({
+            msg: "参数错误",
+            code: 400
+        })
+    }
+
+})
 
 //开启监听
 app.listen(3000, () => console.log('success'))
